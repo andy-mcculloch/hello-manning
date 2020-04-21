ac51e66ff86569a9a91dc9de8b96e94c981b8a27
Andy McCulloch <andy@andymcculloch.co.uk>
2020-04-21 20:19:45 +0100

    added goodbye function

--- handler.js
+++ handler.js
@@ -12,6 +12,18 @@ module.exports.hello = async event => {
     ),
   };
 
+module.exports.goodbye = async event => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Goodbye cruel world!',
+      },
+      null,
+      2
+    ),
+  };
+
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
